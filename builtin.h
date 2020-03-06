8ac0c2b6e24919d68c2f689abb51cd4dd0f851c0
Brooskiey <44653264+Brooskiey@users.noreply.github.com>
2020-03-06 09:23:50 -0500

    Delete builtin.h

--- builtin.h
+++
@@ -1,8 +0,0 @@
-#ifndef BUILTIN_H
-    #define BUILTIN_H
-    void   do_pipe(char** , char** , int*);
-    void   do_file_list(char**);
-    void   do_file_remove(char**);
-    void   do_touch(char**);
-    void   do_history(char**);
-#endif
